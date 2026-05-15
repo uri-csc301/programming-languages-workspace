fd53f3bdf1710f70232532bebdd9004cfb658b7d
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2026-05-15 15:46:39 -0400

    Add skeleton for Prolog predicates

---
+++ self-assess-problems/week-08-prolog/predicates.pl
@@ -0,0 +1,32 @@
+% Week 8 – Prolog Self Assessment
+% Name:
+
+/* =========================
+   Question 1: firstPair
+   ========================= */
+
+% TODO: define firstPair/1
+
+
+
+/* =========================
+   Question 2: del3
+   ========================= */
+
+% TODO: define del3/2
+
+
+
+/* =========================
+   Question 3: halve
+   ========================= */
+
+% TODO: define halve/3
+
+
+
+/* =========================
+   Question 4: myflip
+   ========================= */
+
+% TODO: define myflip/2
