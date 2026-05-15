c3971296b0845c9c51dba6fb7cc917bc3b353aa8
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2026-05-15 15:34:53 -0400

    Add files via upload

---
+++ self-assess-problems/week-08-prolog/family.pl
@@ -0,0 +1,14 @@
+% these are the family facts 
+female(pam).
+female(liz).
+female(ann).
+female(pat).
+male(tom).
+male(bob).
+male(jim).
+parent(pam,bob).   % pam is parent of bob (etc.)
+parent(tom,bob).
+parent(tom,liz).
+parent(bob,ann).
+parent(bob,pat).
+parent(pat,jim).
