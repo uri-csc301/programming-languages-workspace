93e55ff8d8505cae253f9cb935a8f25cf80e961d
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2026-05-14 20:25:19 -0400

    Create Int class with constructor and methods
    Initial implementation of Int class with basic methods.

---
+++ self-assess-problems/week-07-memory/Int.java
@@ -0,0 +1,40 @@
+public class Int {
+    
+    // (a) Field
+    private int value;
+
+    // (b) Constructor
+    public Int(int x) {
+        value = x;
+    }
+
+    // (c) toString method
+    public String toString() {
+        // TODO: return value as a String
+        return "";
+    }
+
+    // (d) plus method
+    public Int plus(Int other) {
+        // TODO: return a new Int object with summed values
+        return null;
+    }
+
+    // (e) minus method
+    public Int minus(Int other) {
+        // TODO
+        return null;
+    }
+
+    // times method
+    public Int times(Int other) {
+        // TODO
+        return null;
+    }
+
+    // div method
+    public Int div(Int other) {
+        // TODO
+        return null;
+    }
+}
